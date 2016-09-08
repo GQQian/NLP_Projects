d32a9be9eb74ea107d61e65a8d062d8122e54b1b
Youdan Xu <woshixuyoudan@163.com>
2016-09-08 17:22:51 -0400

    add global variable

--- pro1/ngram.py
+++ pro1/ngram.py
@@ -3,12 +3,13 @@ import os
 indir_pre = os.getcwd() + "/"
 outdir_pre = os.getcwd() + "/"
 sentence_maxlen = 100
-
+nprob_dic, nhash_dic, ncounter = {}, {}, {}
 
 def preprocess(indir):
     # TODO
     # email, Upper-lower case
     # sentence boundary
+    # ...
 
     pro_content = ""
     for root, dirs, filenames in os.walk(indir):
@@ -19,40 +20,46 @@ def preprocess(indir):
     return pro_content
 
 
+def ntoken_count(n, content):
+    counter = {}
+    tokens = content.split()
+    _len = len(tokens)
+    for i in xrange(_len - n + 1):
+        key = tuple(tokens[i:(i + n)])
+        counter[key] = counter.get(key, 0) + 1
+
+    return counter
+
+
 def ngram_generator(n, content):
-    def ntoken_count(n, content):
-        counter = {}
-        tokens = content.split()
-        _len = len(tokens)
-        for i in xrange(_len - n + 1):
-            key = tuple(tokens[i:(i + n)])
-            counter[key] = counter.get(key, 0) + 1
-
-        return counter
-
-    counter_n = ntoken_count(n, content)
-    prob_dic, hash_dic = {}, {}
+    ncounter[n] = ncounter.get(n, ntoken_count(n, content))
+    nhash_dic[n], nprob_dic[n] = {}, {}
 
     if n == 1:
-        _sum = sum(counter_n.values())
-        prob_dic = dict((key, num * 1.0 / _sum) for key, num in counter_n.items())
+        _sum = sum(ncounter[n].values())
+        nprob_dic[n] = dict((key, num * 1.0 / _sum) for key, num in ncounter[n].items())
     elif n > 1:
-        counter_nminus1 = ntoken_count(n - 1, content)
-        for key_n, num_n in counter_n.items():
+        ncounter[n - 1] = ncounter.get(n - 1, ntoken_count(n - 1, content))
+        for key_n, num_n in ncounter[n].items():
             key_nminus1 = key_n[:-1]
 
-            hash_dic[key_nminus1] = hash_dic.get(key_nminus1, [])
-            hash_dic[key_nminus1].append(key_n)
+            nhash_dic[n][key_nminus1] = nhash_dic[n].get(key_nminus1, [])
+            nhash_dic[n][key_nminus1].append(key_n)
 
-            num_nminus1 = counter_nminus1[key_nminus1]
-            prob_dic[key_n] = 1.0 * num_n / num_nminus1
-
-    return prob_dic, hash_dic
+            num_nminus1 = ncounter[n - 1][key_nminus1]
+            nprob_dic[n][key_n] = 1.0 * num_n / num_nminus1
 
 
-def sentence_generator(prob_dic, hash_dic, pre_sent = ""):
+def sentence_generator(n, prob_dic, hash_dic, pre_sent = ""):
     # TODO:
-    pass
+
+
+
+
+
+    prob_table = {}
+    if n == 1:
+        pass
 
 
 
@@ -74,9 +81,9 @@ def main():
         os.makedirs(outdir)
 
     content = preprocess(indir)
-    prob_dic, hash_dic = ngram_generator(n, content)
-    print prob_dic
-    print hash_dic
+    ngram_generator(n, content)
+    print nprob_dic[n]
+    print nhash_dic[n]
 
 
 if __name__ == "__main__":
