900187a4e89a136a0a8b9b0f1307c38550e94a29
Zili Xiang <zx77@cornell.edu>
2016-09-17 17:05:50 -0400

    main: print all topics, update var names; ngram.py: move variable content to __init__

--- pro1/ngram.py
+++ pro1/ngram.py
@@ -3,7 +3,7 @@ import random
 import operator
 
 class ngram(object):
-    def __init__(self):
+    def __init__(self,content):
         """
         Parameter:
         ncounter_dic: stores the counts for grams, ncounter_dic[1] stores grams for unigram, 
@@ -14,17 +14,17 @@ class ngram(object):
         """
 
         self.nprob_dic, self.nhash_dic, self.ncounter_dic = {}, {}, {}
+        self.content = content
 
 
-    def ntoken_count(self,n, content):
+    def ntoken_count(self,n):
         """ 
         Input: n: when n = 1,2,3 indicates unigram, bigram and trigram respectively;
-                   content: list of input sentences
         Output counter: dictionary, with key, value indicates n-gram tuples, counts respectively
         """
 
         counter = {}
-        tokens = content.split()
+        tokens = self.content.split()
         _len = len(tokens)
         for i in xrange(_len - n + 1):
             key = tuple(tokens[i:(i + n)])
@@ -32,21 +32,20 @@ class ngram(object):
         return counter
 
 
-    def generate_ngram(self,n, content):
+    def generate_ngram(self,n):
         """ 
         Input: n: when n = 1,2,3 indicates unigram, bigram and trigram respectively;
-               content: list of input sentences
         Output self.nprob_dic[n]: dictionary, with key, value indicates n-gram tuples, probability respectively
         """
 
-        self.ncounter_dic[n] = self.ncounter_dic[n] if n in self.ncounter_dic else self.ntoken_count(n, content)
+        self.ncounter_dic[n] = self.ncounter_dic[n] if n in self.ncounter_dic else self.ntoken_count(n)
         self.nhash_dic[n], self.nprob_dic[n] = {}, {}
 
         if n == 1:
             _sum = sum(self.ncounter_dic[n].values())
             self.nprob_dic[n] = dict((key, num * 1.0 / _sum) for key, num in self.ncounter_dic[n].items())
         elif n > 1:
-            self.ncounter_dic[n - 1] = self.ncounter_dic[n - 1] if n - 1 in self.ncounter_dic else self.ntoken_count(n - 1, content)
+            self.ncounter_dic[n - 1] = self.ncounter_dic[n - 1] if n - 1 in self.ncounter_dic else self.ntoken_count(n - 1)
             for key_n, num_n in self.ncounter_dic[n].items():
                 key_nminus1 = key_n[:-1]
 
@@ -59,10 +58,9 @@ class ngram(object):
         return self.nprob_dic[n]
 
 
-    def generate_sentence(self,n, content, sentence = '<s>'):
+    def generate_sentence(self,n, sentence = '<s>'):
         """ 
         Input: n: when n = 1,2,3 indicates unigram, bigram and trigram respectively;
-               content: list of input sentences
 
         Output: normalized_sentence: random sentence when sentence is not set, complete the sentence if
                 sentence is set to some starting words
@@ -72,8 +70,8 @@ class ngram(object):
         sentence_maxlen = 100
  
         # if do not have n-gram result, then choose (n-1)-gram
-        def produce_next(n, content, sentence_list):
-            self.nprob_dic[n] = self.nprob_dic[n] if n in self.nprob_dic else self.generate_ngram(n, content)
+        def produce_next(n, sentence_list):
+            self.nprob_dic[n] = self.nprob_dic[n] if n in self.nprob_dic else self.generate_ngram(n)
             rand_prob = random.uniform(0.0, 1.0)
             prob_sum = 0
 
@@ -86,7 +84,7 @@ class ngram(object):
             else:
                 key = tuple(sentence_list[-n+1:])
                 if key not in self.nhash_dic[n]:
-                    sentence_list = produce_next(n - 1, content, sentence_list)
+                    sentence_list = produce_next(n - 1, sentence_list)
                 else:
                     for token in self.nhash_dic[n][key]:
                         prob_sum += self.nprob_dic[n][tuple(token)]
@@ -104,7 +102,7 @@ class ngram(object):
             sentence_list = sentence.split()
             while len(sentence_list) < sentence_minlen or \
                 (len(sentence_list) < sentence_maxlen and sentence_list[-1] != '</s>'):
-                sentence_list = produce_next(min(len(sentence_list) + 1, n), content, sentence_list)
+                sentence_list = produce_next(min(len(sentence_list) + 1, n), sentence_list)
 
             # normalize: remove '<s>', '<\s>', multiple white spaces
             normalized_sentence = ' '.join(sentence_list).replace('<s>', '').replace('</s>', '')
--- pro1/tasks.py
+++ pro1/tasks.py
@@ -5,23 +5,26 @@ from gt_ngram import gt_ngram
 
 indir_pre = os.getcwd() + "/"
 outdir_pre = os.getcwd() + "/"
+topics = ['atheism', 'autos', 'graphics', 'medicine', 'motorcycles', 'religion', 'space']
 
-def random_sentence_ngram(n = 2, sent_pre = "I have", topic = "autos"):
+def random_sentence_ngram(n = 2, sent_pre = "I have", topics = ["autos"]):
     # TODO: lili write print like first line in ompare_perplexity_ngram()
-    indir = indir_pre + "data/classification_task/{}/train_docs".format(topic)
-    content = preprocess.preprocess_dir(indir)
-    ngrams = ngram()
+    for topic in topics:
 
-    for k in xrange(1, n + 1):
-        print "\n\n[{}-gram]\n".format(k)
+        indir = indir_pre + "data/classification_task/{}/train_docs".format(topic)
+        content = preprocess.preprocess_dir(indir)
+        ngrams = ngram(content)
+        print "\n\n\nTopic: {}\n".format(topic)
+        for k in xrange(1, n + 1):
+            print "[{}-gram]\n".format(k)
 
-        print "Empty sentence"
-        for i in xrange(3):
-            print "[{}]  ".format(i + 1) + ngrams.generate_sentence(k, content)
+            print "Empty sentence"
+            for i in xrange(3):
+                print "[{}]  ".format(i + 1) + ngrams.generate_sentence(k)
 
-        print "\nWith incomplete sentence: " + "\"{}\"".format(sent_pre)
-        for i in xrange(3):
-            print "[{}]  ".format(i + 1) + ngrams.generate_sentence(k, content, sent_pre)
+            print "\nWith incomplete sentence: " + "\"{}\"".format(sent_pre)
+            for i in xrange(3):
+                print "[{}]  ".format(i + 1) + ngrams.generate_sentence(k, sent_pre)
 
 
 def topic_classification():
@@ -51,7 +54,7 @@ def compare_perplexity_ngram():
 
 
 def main():
-    random_sentence_ngram(topic="atheism")
+    random_sentence_ngram(topics=topics)
     compare_perplexity_ngram()
 
 
