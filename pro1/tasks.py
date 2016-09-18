34e8b9a5acab03a0d54f516967607ede3ae0ffbd
Youdan Xu <woshixuyoudan@163.com>
2016-09-17 20:42:36 -0400

    done Topic classification with gt_ngram

--- pro1/tasks.py
+++ pro1/tasks.py
@@ -2,10 +2,13 @@ from ngram import ngram
 import os
 import preprocess
 from gt_ngram import gt_ngram
+import sys
+import operator
+import csv
 
 indir_pre = os.getcwd() + "/"
 outdir_pre = os.getcwd() + "/"
-topics = ['atheism', 'autos', 'graphics', 'medicine', 'motorcycles', 'religion', 'space']
+topics = {'atheism':0, 'autos':1, 'graphics':2, 'medicine':3, 'motorcycles':4, 'religion':5, 'space':6}
 
 def random_sentence_ngram(n = 2, sent_pre = "I have"):
     # TODO: lili write print like first line in ompare_perplexity_ngram()
@@ -42,16 +45,72 @@ def generate_perplexity_gt_ngram():
 
 
 def topic_classification_gt_ngram():
-    gt_ngrams = {}
+    """
+    calculate the accuracy for topic classification with different
+    n in Good-Turing ngram, then choose the best one to classify files
+    in test_for_classification directory, and write results into
+    gt_result.csv in classification_task directory
+    """
+
+    # get gt_ngram for each topic and read all test data
+    gt_ngrams, train_text, test_text  = {}, {}, {} #key: topic
     for topic in topics:
-        indir = indir_pre + "data/classification_task/{}/train_docs".format(topic)
-        content = preprocess.preprocess_dir(indir)
-        gt_ngrams[topic] = gt_ngram(content)
+        train_f = indir_pre + "data/classification_task/{}/train.txt".format(topic)
+        test_f = indir_pre + "data/classification_task/{}/train.txt".format(topic)
+        if not os.path.isfile(train_f) or not os.path.isfile(test_f):
+            split_train_test()
 
+        train_text[topic] = open(train_f, 'r').read()
+        test_text[topic] = open(test_f, 'r').read()
 
+        gt_ngrams[topic] = gt_ngram(train_text[topic])
 
+    # calculate the accuracy for n-gram and choose the best one
+    # accuracy = {} # key: the n in gt_ngram
+    # for i in xrange(1, 2):
+    #     _sum, correct = 0, 0
+    #     for label_topic, text in test_text.items():
+    #         sentences = text.split('</s>')
+    #         for sentence in sentences:
+    #             sentence += ' </s>'
+    #             min_perp, min_topic = sys.maxint, label_topic
+    #
+    #             for topic in topics:
+    #                 perp = gt_ngrams[topic].generate_perplexity(i, sentence)
+    #                 if perp < min_perp:
+    #                     min_perp = perp
+    #                     min_topic = topic
+    #
+    #             if label_topic == min_topic:
+    #                 correct += 1
+    #             _sum += 1
+    #
+    #     accuracy[i] = 1.0 * correct / _sum
+    #     print "[{}-gram] accuracy: {}".format(i, accuracy[i])
+    # #choose the best n
+    # n = max(accuracy.iteritems(), key = operator.itemgetter(1))[0]
+    n = 3
+
+    # get the result for files in test_for_classification directory
     test_dir = indir_pre + "data/classification_task/test_for_classification"
-    test_text = preprocess.preprocess_dir(test_dir)
+    csv_f = indir_pre + "data/classification_task/gt_result.csv"
+
+    with open(csv_f, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = ['ID', 'Prediction'])
+        writer.writeheader()
+
+        for root, dirs, filenames in os.walk(test_dir):
+            for f in filenames:
+                text = preprocess.preprocess_file(os.path.join(root, f))
+                min_perp, min_topic = sys.maxint, ''
+
+                for topic in topics:
+                    perp = gt_ngrams[topic].generate_perplexity(n, text)
+                    if perp < min_perp:
+                        min_perp = perp
+                        min_topic = topic
+
+                writer.writerow({'ID': f, 'Prediction': '{}'.format(topics[min_topic])})
 
 
 def split_train_test():
@@ -78,13 +137,12 @@ def split_train_test():
         open(test_path, 'w').write(test_text)
 
 
-
 def spell_checker_gt_nrgam():
     pass
 
 
 def main():
-    split_train_test()
+    topic_classification_gt_ngram()
 
 
 if __name__ == "__main__":
