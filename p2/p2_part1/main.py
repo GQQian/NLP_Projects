cccd61384d670d7c1f7ac64b367f638738f2f02c
Youdan Xu <woshixuyoudan@163.com>
2016-10-12 17:27:52 -0400

    change pro1 to p1

---
+++ p2/p2_part1/main.py
@@ -0,0 +1,17 @@
+# TODO: 1. read files, parse files
+#       2. feeding data to models
+#       3. getting predictions from models and write to files
+from test import *
+
+
+def main():
+#############################################################################################
+#############################################################################################
+##### replace the function below with whatever function you want to call from test.py #######
+#############################################################################################
+#############################################################################################
+    uncertain_sent_detection_bm()
+    print "hey"
+
+if __name__ == "__main__":
+    main()
